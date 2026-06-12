20b6ad3b8c3c51bde68f777377a42573cdb57945
Mira <291997298+softfaulty@users.noreply.github.com>
2026-06-12 00:27:23 -0700

    add local registry API constants

---
+++ src/plugins/remote/constants.ts
@@ -0,0 +1,25 @@
+/*
+ * Vencord, a modification for Discord's desktop app
+ * Copyright (c) 2026 Vendicated and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+export const REMOTE_PLUGIN_API_VERSION = "1";
+
+// TODO: change this when i get softfault.cc domain
+export const REMOTE_PLUGIN_REGISTRY_URL =
+    "http://localhost:3000/api/plugins";
+
+export const REMOTE_PLUGIN_CRASH_LIMIT = 3;
